b36dcf9329eae6830c82ec3c1b53800c9f32df89
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-12 22:42:49 +0300

    implemets object inheritance

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,12 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
+
+
+my_student = Student("Jerome", "Chauncey")
+my_student.learn("Python")
+
+for k in my_student.get_knowledge():
+    print(f"{my_student.first_name} {my_student.last_name} learnt {k}")
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,16 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Javascript", "C", "Python", "CSS"]
+
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+        # return self.knowledge[0]
+    
+
+teacher = Teacher("Stella", "Margy")
+print(f"{teacher.first_name} {teacher.last_name} teaches: {teacher.teach()}")
+# print(teacher.teach())
+        
